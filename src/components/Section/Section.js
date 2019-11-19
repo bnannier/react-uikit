c8b73430ae7e3dadad0387e8cf918a9ff45a824e
Bobby Nannier <bobby.nannier@prodigylabs.net>
2019-11-19 12:16:59 -0500

    Typography import complete

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './section.scss';
 
-const Section = ({children}) =>{
+const Section = (props) =>{
     return (
-        <div className="uk-section">{children}</div>
+        <div className={applyStyles(props)}>{props.children}</div>
     )
+};
+
+const applyStyles = (props) => {
+    return classNames("uk-section",
+        {"uk-section-muted": props.muted},
+        {"uk-section-primary": props.primary},
+        {"uk-section-secondary": props.secondary})
+};
+
+Section.propTypes = {
+    /** Adds a muted background color */
+    muted: PropTypes.bool,
+    /** Adds a primary background color */
+    primary: PropTypes.bool,
+    /** Adds a secondary background color */
+    secondary: PropTypes.bool,
+    /** Pass children elements to this element */
+    children: PropTypes.node
+};
+
+Section.defaultProps = {
+    muted: false,
+    primary: false,
+    secondary: false,
+    children: undefined
 };
 
 export default Section;
--- src/components/Section/Section.md
+++ src/components/Section/Section.md
@@ -1,8 +1,17 @@
+https://getuikit.com/docs/section
 
-Section
+### Create horizontal layout sections with different background colors and styles.
 
+## Usage
+Sections are used to separate the content of a page into differently styled blocks. To apply this component, just add the style modifier props to a container element. You can place a container from the Container component to modify the width of the content inside sections and add horizontal padding. Note that the padding of a nested container will be reset.
+
+### Style modifiers
 ```js
-<Section>
-    <div>test</div>
-</Section>
+<React.Fragment>
+    <Section>Default</Section>
+    <Section muted>Muted</Section>
+    <Section primary>Primary</Section>
+    <Section secondary>Secondary</Section>
+</React.Fragment>
 ```
+By default, a section is blank. That is why it is important to add a modifier class for styling. In our example we are using the muted props.
